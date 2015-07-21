d61a1a5cb521b466bea266c354766b1970d0e243
Arıl Bozoluk <arilbozoluk@hotmail.com>
2015-07-22 00:28:38 +0300

    Dropbox serverside api kullanımı

--- assets/js/ng/app.js
+++ assets/js/ng/app.js
@@ -1,5 +1,12 @@
 'use strict';
-
+var Dropbox = require("dropbox");
 angular.module('zopaApp', [
 
-]);
+])
+.run(function(){
+  var client = new Dropbox.Client({
+      key: "ji7q0ylp1na9x1q",
+      secret: "g9rvglf4mn6sv5b"
+  });
+  console.log(client);
+});
